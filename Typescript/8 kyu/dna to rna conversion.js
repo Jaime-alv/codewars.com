5232ec79f085aa3eccf01f512323ae615f1e8355
Jaime Álvarez <jaime.alv.fdz@gmail.com>
2021-12-30 19:11:27 +0100

    Add files via upload

---
+++ Typescript/8 kyu/dna to rna conversion.js
@@ -0,0 +1,30 @@
+/*
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological
+systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'),
+Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly
+from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced
+by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty.
+All input is guaranteed to be valid, i.e. each input string will only ever consist
+of 'G', 'C', 'A' and/or 'T'.
+*/
+function DNAtoRNA(dna) {
+    var result = [];
+    for (var _i = 0, dna_1 = dna; _i < dna_1.length; _i++) {
+        var character = dna_1[_i];
+        if (character == 'T')
+            result.push('U');
+        else
+            result.push(character);
+    }
+    return result.join('');
+}
+console.log(DNAtoRNA('TTTT'));
